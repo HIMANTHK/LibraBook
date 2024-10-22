d03299136ecae094e32c9b86129fb342121c93f8
Himanthk <himanthk97@gmail.com>
2024-10-22 09:40:32 +0530

    Re-added files from LibraBook folder

---
+++ LibraBook/src/main/java/com/librabook/LibrabookApplication.java
@@ -0,0 +1,13 @@
+package com.librabook.librabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraBookApplication.class, args);
+	}
+
+}
